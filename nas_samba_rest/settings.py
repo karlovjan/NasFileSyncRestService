adea1c1b76a9d50d71a89b19d384e90fcbc9663a
mbaros <mbaros@email.com>
2021-02-21 21:51:01 +0100

    docs and host name change

--- nas_samba_rest/settings.py
+++ nas_samba_rest/settings.py
@@ -2,7 +2,7 @@
 # default app configuration
 ENV = 'development'
 DEBUG = True
-SERVER_NAME = '127.0.0.1:5001'
+SERVER_NAME = '0.0.0.0:5001'
 # upload max 1 GB file
 MAX_CONTENT_LENGTH = 1 * 1024 * 1024 * 1024
 # MY APP SETTINGS
